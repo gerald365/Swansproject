cfbe5ed3e28f4dfaa73265a7b6e3ee9a50f10ef0
102720546 <54521510+102720546@users.noreply.github.com>
2020-04-19 14:53:46 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color: lightblue;
+}
 
-echo "Hello World!";
-?>
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
+</style>
+</head>
+<body>
+
+<h1>My Transportation Safety APP </h1>
+<p>This is a test paragraph.</p>
+
+</body>
+</html>
