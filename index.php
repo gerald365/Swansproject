98d7ee1299e6f4b0dc0390eca028ab30f3e8a6d9
gerald365 <62080910+gerald365@users.noreply.github.com>
2020-04-27 15:10:25 +1000

    from previous php code

--- index.php
+++ index.php
@@ -1,26 +1,40 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="en">
 <head>
-<style>
-body {
-  background-color: lightblue;
-}
-
-h1 {
-  color: white;
-  text-align: center;
-}
-
-p {
-  font-family: verdana;
-  font-size: 20px;
-}
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<style type="text/css">
+    .bs-example{
+        margin: 10px;
+    }
 </style>
 </head>
 <body>
+<a href="#" class="navbar-left"><img src="pictures/download.jpg"></a>
 
-<h1>My Transportation Safety APP </h1>
-<p>This is a test app in Azure environment.</p>
-
+<div class="bs-example">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="Consignment.php" class="nav-item nav-link active">Consignments</a>
+                <a href="Asset.php" class="nav-item nav-link">Asset Conditions</a>
+                <a href='Manageuser.php' class="nav-item nav-link">Manage Users</a>
+            </div>
+            <form class="form-inline ml-auto">
+                <input type="text" class="form-control mr-sm-2" placeholder="Search">
+                <button type="submit" class="btn btn-outline-light">Search</button>
+            </form>
+        </div>
+    </nav>
+</div>
 </body>
-</html>
+</html> 
